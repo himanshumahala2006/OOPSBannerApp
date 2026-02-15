63f377bfbdba5ae40e18b1a1c18049bba01d7f36
Himanshu Mahala <163985852+himanshumahala2006@users.noreply.github.com>
2026-02-15 15:29:27 +0000

    Initial commit

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * UC1: Prints the literal text "OOPS" to the console.
+ * 
+ * @author [Your Name]
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // Print the literal text "OOPS"
+        System.out.println("OOPS");
+    }
+}
